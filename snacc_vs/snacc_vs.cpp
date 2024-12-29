a5cd18729b940493d13eecf7b765a02ec4104b88
merkelmarrow <marco.a.blackwell@gmail.com>
2024-12-29 21:41:54 +0000

    Visual Studio Project created
    Configured as CMake project.

---
+++ snacc_vs/snacc_vs.cpp
@@ -0,0 +1,12 @@
+﻿// snacc_vs.cpp : Defines the entry point for the application.
+//
+
+#include "snacc_vs.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ snacc_vs/snacc_vs.h
@@ -0,0 +1,8 @@
+﻿// snacc_vs.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
