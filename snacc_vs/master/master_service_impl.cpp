1f56c1de0ff2dce14fe33c8f679c0bf3c1686a5f
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-04 21:15:15 +0000

    Added method to HBServiceImpl to get WorkerConnStatus
    Enums introduced to describe worker connection status and system health, finished preliminary logic for heartbeat response override. Wrote method to get worker connection status based on time since last heartbeat.

--- snacc_vs/master/master_service_impl.cpp
+++ snacc_vs/master/master_service_impl.cpp
@@ -17,6 +17,23 @@
 //        const ::heartbeat::HeartbeatRequest* request, ::heartbeat::HeartbeatReply* response);
 // };
 
+enum class WorkerConnStatus {
+	CONNECTED,
+	DISCONNECTED,
+	CONNECTING,
+	CONNECTION_LOST,
+	UNKNOWN
+};
+
+enum class WorkerHealth {
+	HEALTHY,
+	DEGRADED,
+	UNREACHABLE,
+	FAILED,
+	MAINTENANCE,
+	UNKNOWN
+};
+
 class HeartbeatServiceImpl final : public heartbeat::HeartbeatService::Service {
 private:
 	// Storage for worker states
@@ -33,5 +50,38 @@ public:
 			std::lock_guard<std::mutex> lock(mutex_);
 			last_heartbeat_[request->worker_id()] = now;
 		}
+
+		enum HeartbeatResponse {
+			CONTINUE,
+			STOP
+		};
+
+		//// TODO: INSERT LOGIC TO EVALUATE HEALTH OF WORKER AND SEND REPONSE CODE
+		int32_t response_code = CONTINUE;
+		////
+
+		response->set_acknowledged(response_code);
+
+		return ::grpc::Status::OK;
 	}
+
+	WorkerConnStatus GetWorkerConnStatus(int32_t worker_id) {
+		constexpr int DISCONNECTED_THRESHOLD = 30;
+		constexpr int CONNECTION_LOST_THRESHOLD = 15;
+
+		std::lock_guard lock(mutex_);
+
+		if (auto it = last_heartbeat_.find(worker_id); it != last_heartbeat_.end()) {
+			auto now = std::chrono::steady_clock::now();
+			auto diff = std::chrono::duration_cast<std::chrono::seconds>(now - it->second).count();
+
+			if (diff > DISCONNECTED_THRESHOLD) return WorkerConnStatus::DISCONNECTED;
+			if (diff > CONNECTION_LOST_THRESHOLD) return WorkerConnStatus::CONNECTION_LOST;
+			return WorkerConnStatus::CONNECTED;
+		}
+
+		return WorkerConnStatus::UNKNOWN;
+	}
+	
+	
 };
