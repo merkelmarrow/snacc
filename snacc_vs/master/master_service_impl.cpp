4dd60326e29d45c1ceb540a86bb079592159addf
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-04 20:12:57 +0000

    Begin implementing HeartbeatServiceImpl class

--- snacc_vs/master/master_service_impl.cpp
+++ snacc_vs/master/master_service_impl.cpp
@@ -1,11 +1,37 @@
 // master/master_service_impl.cpp
 
-#include "heartbeat.proto"
-#include <stdlib>
+#include "heartbeat.grpc.pb.h"
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <cstdint>
+#include <unordered_map>
 
-void healthcheck_loop(HeartbeatService* hbservice, bool& stop_loop) {
-	if (stop_loop) return;
-	std::chrono::timepoint current_time;
-	current_time = std::chrono::system_clock::now();
-}
+//// For reference (namespace heartbeat)
+// class Service : public ::grpc::Service {
+// public:
+//    Service();
+//    virtual ~Service();
+//    // Worker calls this every 10s to report health
+//    virtual ::grpc::Status SendHeartbeat(::grpc::ServerContext* context, 
+//        const ::heartbeat::HeartbeatRequest* request, ::heartbeat::HeartbeatReply* response);
+// };
+
+class HeartbeatServiceImpl final : public heartbeat::HeartbeatService::Service {
+private:
+	// Storage for worker states
+	std::mutex mutex_;
+	std::unordered_map<int32_t, std::chrono::steady_clock::time_point> last_heartbeat_; // workerID (int32_t), timestamp (time_point)
+
+public:
+	::grpc::Status SendHeartbeat(::grpc::ServerContext* context,
+		const ::heartbeat::HeartbeatRequest* request, ::heartbeat::HeartbeatReply* response) override {
+		// 1. Get current timestamp
+		auto now = std::chrono::steady_clock::now();
+
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			last_heartbeat_[request->worker_id()] = now;
+		}
+	}
+};
