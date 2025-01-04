69d2304d19c114d5a709b49edd6b2d9361cefb8d
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-04 19:26:32 +0000

    Linked Zlib and fixed CMake presets issue (painful)
    It took me 2 hours to figure out why find_package couldn't find ZLIB. It's because you have to set "VCPKG_ROOT" = "$env{VCPKG_ROOT}" in CMakePresets.json file. Certain libraries like zlib and openssl (installed with vcpkg) seem to look for that variable in CMake instead of inferring it elsewhere.

---
+++ snacc_vs/master/master_main.cpp
@@ -0,0 +1,1 @@
+// master/master_main.cpp
--- snacc_vs/master/master_service_impl.cpp
+++ snacc_vs/master/master_service_impl.cpp
@@ -1,5 +1,11 @@
-// master_service_impl.cpp
+// master/master_service_impl.cpp
 
 #include "heartbeat.proto"
+#include <stdlib>
 #include <chrono>
-#include <mutex>
+
+void healthcheck_loop(HeartbeatService* hbservice, bool& stop_loop) {
+	if (stop_loop) return;
+	std::chrono::timepoint current_time;
+	current_time = std::chrono::system_clock::now();
+}
