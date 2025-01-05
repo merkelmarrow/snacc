08afc8a1dfb7a900314ac2c2b1d8b179148280b2
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-05 17:25:06 +0000

    Created read_file func in master_main.cpp
    Created read_file function in master_main.cpp in order to read keys from files.

--- snacc_vs/master/master_main.cpp
+++ snacc_vs/master/master_main.cpp
@@ -1,1 +1,27 @@
 // master/master_main.cpp
+
+
+#include "master_service_impl.cpp"
+#include "grpcpp/grpcpp.h"
+#include "grpcpp/security/server_credentials.h"
+#include "grpcpp/security/credentials.h"
+
+#include <thread>
+#include <chrono>
+
+#include <filesystem>
+#include <fstream>
+
+std::string read_file (const std::string& filename) {
+	std::filesystem::path filepath(filename);
+	if (!std::filesystem::exists(filepath)) {
+		throw std::runtime_error("File not found: " + filename);
+	}
+	
+	std::ifstream file(filepath, std::ios::binary);
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open file: " + filename);
+	}
+
+	return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+}
