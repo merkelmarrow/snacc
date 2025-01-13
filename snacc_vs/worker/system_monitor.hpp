113ebe013e602ca542893fa7cd75be497cbfde61
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-13 20:32:43 +0000

    Introduced interface for reading system metrics

--- snacc_vs/worker/system_monitor.hpp
+++ snacc_vs/worker/system_monitor.hpp
@@ -1,24 +1,36 @@
 // worker/system_monitor.hpp
+
 #pragma once
+
 #include <pdh.h>
 #include <string>
 #include <windows.h>
 
-class SystemMonitor {
+// Interface for monitoring system metrics
+class ISystemMonitor {
+public:
+	virtual ~ISystemMonitor() = default;
+	virtual bool Initialise() = 0;
+	virtual float GetCPUUsage() = 0;
+	virtual float GetMemoryUsage() = 0;
+	virtual std::string GetLastError() const = 0;
+};
+
+class SystemMonitor : public ISystemMonitor {
+public:
+	SystemMonitor();
+	~SystemMonitor() override;
+
+	bool Initialise() override;
+	float GetCPUUsage() override;
+	float GetMemoryUsage() override;
+	std::string GetLastError() const override { return last_error_; }
+
 private:
-	PDH_HQUERY cpu_query_{ NULL };
-	PDH_HCOUNTER cpu_counter_{ NULL };
+	PDH_HQUERY cpu_query_{ nullptr };
+	PDH_HCOUNTER cpu_counter_{ nullptr };
 	std::string last_error_;
 
 	void Cleanup();
 	void SetLastError(const std::string& error);
-
-public:
-	SystemMonitor();
-	~SystemMonitor();
-
-	bool Initialise();
-	float GetCPUUsage();
-	float GetMemoryUsage();
-	std::string GetLastError() const { return last_error_; }
 };
