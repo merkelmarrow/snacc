2430b0e5a1ce6aef392589cfd8c6549886c0deff
merkelmarrow <marco.a.blackwell@gmail.com>
2025-01-20 18:39:57 +0000

    Switched to using TinyProcessLib for safety

--- snacc_vs/main.cpp
+++ snacc_vs/main.cpp
@@ -1,13 +1,29 @@
 // main.cpp
 
 /// Program to practice executing powershell commands
-#include <iostream>
-#include <string>
+#include <iostream> // std::cout, std::cerr, std::endl
+#include <string> // std::string
+#include <cstdlib> // std::system
+#include "process.hpp" // TinyProcessLib::Process
+
+namespace {
+	namespace tpl = TinyProcessLib; // Shorten TinyProcessLib to tpl
+}
 
 int main() {
 	std::string command = "powershell -Command \"Write-Host 'Hi'\"";
 
-	int result = std::system(command.c_str());
+	tpl::Process process(command,
+		"",
+		[](const char* bytes, size_t n) {
+			std::cout << "Output: " << std::string(bytes, n);
+		},
+		[](const char* bytes, size_t n) {
+			std::cerr << "Error: " << std::string(bytes, n);
+		}
+	);
+
+	auto result = process.get_exit_status();
 
 	if (result == 0) {
 		std::cout << "Success." << std::endl;
